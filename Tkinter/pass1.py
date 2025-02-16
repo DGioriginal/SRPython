a784c921d9c5599dacfeb40e63bd8b50d4061bf4
Ukraine_Grivnya|DGH <117117264+DGioriginal@users.noreply.github.com>
2025-02-16 18:52:46 +0100

    Add files via upload

---
+++ Tkinter/pass1.py
@@ -0,0 +1,8 @@
+def get_db_config():
+    return {
+        'host': 'localhost',
+        'user': 'postgres',
+        'password': '123456',
+        'dbname': 'postgres',
+        'port': '5432'
+    }
